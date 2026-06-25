7680dfa56a49fc021daf45fa7821fc90b24c9d06
TatiBotHR <tanya2gb@rambler.ru>
2026-06-25 20:18:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
+import os
 from telegram import Update
 from telegram.ext import Application, MessageHandler, CommandHandler, ContextTypes, filters
+
+TOKEN = os.getenv("TELEGRAM_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Здравствуйте 👋\n\nОтправьте резюме")
@@ -7,7 +10,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def handle_doc(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Файл получен. Анализирую... 🤖")
 
-app = Application.builder().token("TELEGRAM_TOKEN").build()
+app = Application.builder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.Document.ALL, handle_doc))
